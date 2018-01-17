a8055203f939528f2994880def56f7d6d86d95ac
kdcarbullido <kdcarbullido@gmail.com>
2018-01-17 13:19:04 -0600

    New music selections for Junior!

--- src/data/albums.js
+++ src/data/albums.js
@@ -1,27 +1,27 @@
 export default [{
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    releaseInfo: '1909 Spanish Records',
+    title: 'Miscellaneous Selections',
+    artist: 'Coldplay',
+    releaseInfo: 'Not Applicable',
     albumCover: '/assets/images/album_covers/01.jpg',
     slug: 'the-colors',
     songs: [
-        { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
-        { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
-        { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
-        { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
-        { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+        { title: 'In My Place', duration: '210.00', audioSrc: '/assets/music/02 In My Place.mp3' },
+        { title: 'Moving to Mars', duration: '259.00', audioSrc: '/assets/music/03 Moving to Mars.mp3' },
+        { title: 'Yellow', duration: '269.00', audioSrc: '/assets/music/05 Yellow.mp3' },
+        { title: 'Politik', duration: '319.00', audioSrc: '/assets/music/01 Politik.mp3' },
+        { title: 'The Scientist', duration: '309.00', audioSrc: '/assets/music/04 The Scientist.mp3' }
     ]
 }, {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    releaseInfo: '1909 EM',
+    title: 'Miscellaneous Selectinos',
+    artist: 'Green Day',
+    releaseInfo: 'Not applicable',
     albumCover: '/assets/images/album_covers/02.jpg',
     slug: 'the-telephone',
     songs: [
-        { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
-        { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
-        { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
-        { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
-        { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+        { title: 'When I Come Around', duration: '178.00', audioSrc: '/assets/music/06 When I Come Around.mp3' },
+        { title: 'Brain Stew', duration: '193.00', audioSrc: '/assets/music/10 Brain Stew.mp3' },
+        { title: 'Good Riddance (Time of Your Life)', duration: '154.00', audioSrc: '/assets/music/15 Good Riddance (Time of Your Life).mp3' },
+        { title: 'Know Your Enemy', duration: '191.00', audioSrc: '/assets/music/03 Know Your Enemy.mp3' },
+        { title: '21 Guns', duration: '321.00', audioSrc: '/assets/music/16 21 Guns.mp3' }
     ]
 }];
